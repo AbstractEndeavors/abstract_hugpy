8348a09057d708167d6a6df01aec8d857d7ebdf4
AbstractEndeavors <partners@abstractendeavors.com>
2025-09-18 07:12:27 -0500

    Deploy version 0.0.0.54 at 2025-09-18 12:12:27 UTC

--- build/lib/abstract_hugpy/video_utils.py
+++ build/lib/abstract_hugpy/video_utils.py
@@ -206,7 +206,7 @@ class VideoDirectoryManager(metaclass=SingletonMeta):
     def get_captions(self, video_url):
         data = self.get_data(video_url)
         if not os.path.isfile(data['srt_path']):
-            whisper = self.get_whisper(video_url)
+            whisper = self.get_whisper_result(video_url)
             export_srt(whisper.get('segments', []), data['srt_path'])
             data['captions'] = safe_load_from_file(data['srt_path'])
         return data['captions']

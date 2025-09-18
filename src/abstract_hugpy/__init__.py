6309faa34e567c2cb9f6bd6b64a6206853d3c7d5
AbstractEndeavors <partners@abstractendeavors.com>
2025-09-18 12:01:42 -0500

    Deploy version 0.0.0.63 at 2025-09-18 17:01:42 UTC

--- src/abstract_hugpy/__init__.py
+++ src/abstract_hugpy/__init__.py
@@ -4,4 +4,3 @@ from .hugging_face_models import *
 
 
 
-
--- tester.py
+++ tester.py
@@ -1,4 +1,4 @@
-from src.abstract_hugpy.video_utils import *
+from src.abstract_hugpy.video_console import *
 url = 'https://youtu.be/KN8nJFLu1Rk?list=RDKN8nJFLu1Rk'
 all_data = get_all_data(url)
 input(all_data)

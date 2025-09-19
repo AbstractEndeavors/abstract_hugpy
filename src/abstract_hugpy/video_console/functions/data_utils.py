ff4aafa51b9083ac8dec9fcc61772066c7b208c1
AbstractEndeavors <partners@abstractendeavors.com>
2025-09-18 23:55:06 -0500

    Deploy version 0.0.0.71 at 2025-09-19 04:55:06 UTC

--- src/abstract_hugpy/video_console/functions/data_utils.py
+++ src/abstract_hugpy/video_console/functions/data_utils.py
@@ -49,7 +49,7 @@ def init_data(self, video_url, video_id):
     total_data_path = os.path.join(dir_path, 'total_data.json') 
     thumbnails_dir = os.path.join(dir_path, 'thumbnails')
     os.makedirs(thumbnails_dir, exist_ok=True)
-    video_info = for_dl_video(url=video_url, preferred_format="mp4",dir_path,download_video=False)
+    video_info = for_dl_video(url=video_url, preferred_format="mp4",download_directory=dir_path,download_video=False)
     video_id = video_info.get('id')
     video_basename = f"{video_id}.mp4"
     video_path = os.path.join(dir_path,video_basename)

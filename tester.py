7263d7ef1e3b75704060fdea3f70188611d329e2
AbstractEndeavors <partners@abstractendeavors.com>
2025-09-25 00:31:55 -0500

    Deploy version 0.0.0.87 at 2025-09-25 05:31:55 UTC

--- tester.py
+++ tester.py
@@ -1,6 +1,6 @@
 from src.abstract_hugpy.video_console import get_all_data
 from abstract_apis import *
-url = 'https://typicallyoutliers.com/api/video_url/get_video_metadata_path'
+url = 'https://typicallyoutliers.com/api/video_url/get_all_data'
 all_data = requests.post(url,data={"url":'https://www.youtube.com/shorts/rLlWcvLBluI'})
 input(all_data)
 

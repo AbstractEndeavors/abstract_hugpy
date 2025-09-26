65289430affb1e84182a93da194358f9ab198d56
AbstractEndeavors <partners@abstractendeavors.com>
2025-09-26 18:25:42 -0500

    Deploy version 0.0.0.98 at 2025-09-26 23:25:42 UTC

--- build/lib/abstract_hugpy/hugpy_deepcoder_wsgi.py
+++
@@ -1,4 +0,0 @@
-from hugpy_console.hugpy_flasks.hugpy_deepcoder_flask_app import hugpy_deepcoder_app
-
-app = hugpy_deepcoder_app()
-
--- src/abstract_hugpy/hugpy_proxyvideo_wsgi.py
+++
@@ -1,4 +0,0 @@
-from hugpy_console.hugpy_flasks.hugpy_proxyvideo_flask_app import hugpy_proxyvideo_app
-
-app = hugpy_proxyvideo_app()
-

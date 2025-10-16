c98a57ff57448917573733db2555c7874329ea54
AbstractEndeavors <partners@abstractendeavors.com>
2025-10-16 03:20:26 -0500

    Deploy version 0.0.0.154 at 2025-10-16 08:20:26 UTC

--- build/lib/abstract_hugpy/hugpy_console/hugpy_flasks/__init__.py
+++ build/lib/abstract_hugpy/hugpy_console/hugpy_flasks/__init__.py
@@ -2,3 +2,4 @@ from .hugpy_deepcoder_flask_app import *
 from .hugpy_zerosearch_flask_app import *
 from .hugpy_proxyvideo_flask_app import *
 from .hugpy_video_flask_app import *
+from .hugpy_all_flask_app import *
